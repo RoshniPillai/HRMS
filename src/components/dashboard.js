a5f63992289c4c33296ad8ae8ecd15fd9fec1025
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-02-13 01:39:26 +0530

    appbar

---
+++ src/components/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavigationBar from "./navBar";
+
+export default function Dashboard() {
+  return (
+    <React.Fragment>
+      <NavigationBar />
+    </React.Fragment>
+  );
+}
