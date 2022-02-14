41181cd38bd5cd1db72dbf308986ed8ea9ee3ea7
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-02-14 14:46:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
 import Dashboard from "./components/dashboard";
+import DashboardWIthDrawer from './components/dashboardwithdrawer';
+import EmployeeDirectoryPage from './components/employeeDirectory';
 
 export default function App() {
   return (
     <div className="App">
       <Dashboard />
+      {/* <DashboardWIthDrawer /> */}
+      {/* <EmployeeDirectoryPage /> */}
     </div>
   );
 }
