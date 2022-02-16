bd7eaa9820b886164f4ca3d1282a52a5a62e9d57
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-02-17 02:04:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./styles.css";
+import Dashboard from "./components/dashboard";
+import DashboardWIthDrawer from "./components/dashboardwithdrawer";
+import EmployeeDirectoryPage from "./components/employeeDirectory";
+import EmployeeManagementPage from "./components/employeeManagement";
 import Sample from "./components/sample";
+
 export default function App() {
   return (
     <div className="App">
-      <Sample />
+      {/* <Sample /> */}
+      {/* <Dashboard /> */}
+      {/* <DashboardWIthDrawer /> */}
+      {/* <EmployeeDirectoryPage /> */}
+      <EmployeeManagementPage />
     </div>
   );
 }
--- src/components/employeeManagement.js
+++ src/components/employeeManagement.js
@@ -1,16 +1,12 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import Box from "@mui/material/Box";
+import Drawer from "@material-ui/core/Drawer";
 import Paper from "@mui/material/Paper";
+import Button from "@material-ui/core/Button";
 import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import { makeStyles } from "@mui/styles";
-
-import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
-import NavigationBar from "./navBar";
-import DrawerPage from "./drawer";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import EventIcon from "@mui/icons-material/Event";
@@ -21,85 +17,134 @@ import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import IconButton from "@mui/material/IconButton";
 import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
-import TextField from "@mui/material/TextField";
 import InputLabel from "@mui/material/InputLabel";
 import DatePicker from "./datePicker";
 import Fab from "@mui/material/Fab";
-import AddIcon from "@mui/icons-material/Add";
-import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
 import ChevronLeftOutlinedIcon from "@mui/icons-material/ChevronLeftOutlined";
 import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
-import MenuDrawer from "./menuDrawer";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "pink",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary
-}));
-const useStyles = makeStyles({
+import PrimarySearchAppBar from "./appBar";
+import NavItems from "./NavItems";
+
+const styles = (theme) => ({
+  // The main flex container for the app's layout. Its min-height
+  // is set to `100vh` so it always fill the height of the screen.
   root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    height: "100vh"
-    // border: 0,
-    // borderRadius: 3,
-    // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    // color: "white",
-    // height: 48,
-    // padding: "0 30px"
+    display: "flex",
+    minHeight: "100vh",
+    zIndex: 1,
+    position: "relative",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.default
+  },
+  appBar: {
+    // For full-height drawer
+    //  maxWidth: `calc(100% - ${theme.layout.drawerWidth}px)`
+    maxWidth: `calc(100% - 100px)`
+  },
+  toolbarActions: {
+    marginLeft: "auto"
+  },
+  drawer: {
+    // width: theme.layout.drawerWidth
+    width: 100
+  },
+  drawerPaper: {
+    width: "inherit"
   }
 });
 
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     root: {
-//       position: "fixed",
-//       height: "100vh",
-//       top: 0,
-//       right: 0,
-//       overflowY: "auto",
-//       width: `calc(100vw - ${theme.spacing(30)}px)`, // - Navigation drawer
-//       [theme.breakpoints.down("md")]: {
-//         width: "100vw"
-//       },
-//       backgroundColor: theme.palette.background.default,
-//       display: "flex",
-//       alignItems: "center",
-//       flexDirection: "column",
-//       zIndex: 1150,
-//       border: "4px solid red"
-//     }
-//   })
-// );
-
-export default function EmployeeManagementPage() {
-  const classes = useStyles();
+function EmployeeManagementPage(props) {
+  const { classes } = props;
   return (
-    // <Button className={classes.root}>Hook</Button>
-    // <Container max-width="xl">
-    <Box sx={{ flexGrow: 1 }}>
-      {/* <Button className={classes.root}>Hook</Button> */}
-      <Grid container spacing={1}>
-        <Grid item xs={1} lg={1}>
-          xs=2
-        </Grid>
-        <Grid item xs={11} lg={11}>
-          <Grid container spacing={0.5} className={classes.root}>
-            <Grid item xs={12} lg={12}>
-              <Grid
-                container
-                spacing={0.5}
-                style={{ border: "50px solid" }}
-              ></Grid>
+    <div className={classes.root}>
+      <PrimarySearchAppBar />
+      <Drawer
+        variant="permanent"
+        className={classes.drawer}
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <NavItems />
+        {/* <main className={classes.appContent}>drawer content</main> */}
+      </Drawer>
+      <Box
+        p={3}
+        sx={{
+          flexGrow: 1,
+          paddingTop: "74px",
+          backgroundColor: "#F2F3F5"
+        }}
+        classes={classes.main}
+      >
+        <div
+          role="presentation"
+          //onClick={}
+        >
+          <Breadcrumbs aria-label="breadcrumb">
+            <Typography variant="body2">
+              {" "}
+              <Link underline="hover" color="inherit" href="/">
+                Employee
+              </Link>
+            </Typography>
+
+            <Typography variant="body2">EmployeeManagement</Typography>
+          </Breadcrumbs>
+        </div>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Grid container>
+              <Grid item xs={9}>
+                <Typography variant="h6" component="h6">
+                  Employee Management
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Box display="flex" justifyContent="flex-end">
+                  <Button variant="outlined" color="primary" size="small">
+                    Add New Employee
+                  </Button>
+                  {/* </Box>
+              </Grid> */}
+                  {/* <Grid item xs={1}>
+                <Box display="flex" justifyContent="flex-end"> */}
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    size="small"
+                    style={{ marginLeft: 10, padding: 2 }}
+                  >
+                    ...
+                  </Button>
+                </Box>
+              </Grid>
             </Grid>
-            {/* <Grid item xs={12} lg={12}>
-               xs=12
-            </Grid> */}
           </Grid>
+          <Grid item xs={12}></Grid>
+          <Grid item xs={12}></Grid>
+          <Grid item xs={12}></Grid>
+          {/* <Grid item xs={4}>
+            <Grid container>
+              <Grid item xs={9}>
+                <Typography variant="body1" component="h6">
+                  Who's Off
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="body1" component="h6" align="right">
+                  0 employees
+                </Typography>
+              </Grid>
+            </Grid>
+            <Box mt={2}></Box>
+            <Paper sx={{ padding: "30px 0" }}>
+              <Typography align="center">No one's Off!</Typography>
+            </Paper>
+          </Grid> */}
         </Grid>
-      </Grid>
-    </Box>
-    // </Container>
+      </Box>
+    </div>
   );
 }
+export default withStyles(styles)(EmployeeManagementPage);
