b80f521d45b10ebb33b5de2447d021a3ce827ae3
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-02-21 14:42:23 +0530

    new page added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import DashboardWIthDrawer from "./components/dashboardwithdrawer";
 import EmployeeDirectoryPage from "./components/employeeDirectory";
 import EmployeeManagementPage from "./components/employeeManagement";
 import DashboardPage from "./components/sample";
-
+import LoginPage from "./components/login";
 export default function App() {
   return (
     <div className="App">
-      <DashboardPage />
+      {/* <SignIn /> */}
+      <LoginPage />
+      {/* <DashboardPage /> */}
       {/* <DashboardWIthDrawer /> */}
       {/* <EmployeeDirectoryPage /> */}
       {/* <EmployeeManagementPage /> */}
