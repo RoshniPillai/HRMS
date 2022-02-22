831a97372ffe4e2036d6f9496d16db861f79b355
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-02-22 11:45:10 +0530

    commented router

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
-// import "./styles.css";
-// import DashboardWIthDrawer from "./components/dashboardwithdrawer";
-// import EmployeeDirectoryPage from "./components/employeeDirectory";
-// import EmployeeManagementPage from "./components/employeeManagement";
-// import DashboardPage from "./components/sample";
-// import LoginPage from "./components/login";
-// export default function App() {
-//   return (
-//     <div className="App">
-//       {/* <SignIn /> */}
-//       <LoginPage />
-//       {/* <DashboardPage /> */}
-//       {/* <DashboardWIthDrawer /> */}
-//       {/* <EmployeeDirectoryPage /> */}
-//       {/* <EmployeeManagementPage /> */}
-//     </div>
-//   );
-// }
-
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./components/login";
-import DashboardPage from "./components/sample";
+import "./styles.css";
+import DashboardWIthDrawer from "./components/dashboardwithdrawer";
 import EmployeeDirectoryPage from "./components/employeeDirectory";
 import EmployeeManagementPage from "./components/employeeManagement";
-import Home from "./Home";
-import Layout from "./Layout";
-
+import DashboardPage from "./components/sample";
+import LoginPage from "./components/login";
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="loginPage" element={<LoginPage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route
-            path="employeeDirectoryPage"
-            element={<EmployeeDirectoryPage />}
-          />
-          <Route
-            path="employeeManagementPage"
-            element={<EmployeeManagementPage />}
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      {/* <LoginPage /> */}
+      <DashboardPage />
+      {/* <DashboardWIthDrawer /> */}
+      {/* <EmployeeDirectoryPage /> */}
+      {/* <EmployeeManagementPage /> */}
+    </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// import ReactDOM from "react-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import LoginPage from "./components/login";
+// import DashboardPage from "./components/sample";
+// import EmployeeDirectoryPage from "./components/employeeDirectory";
+// import EmployeeManagementPage from "./components/employeeManagement";
+// import Home from "./Home";
+// import Layout from "./Layout";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           <Route path="loginPage" element={<LoginPage />} />
+//           <Route path="dashboard" element={<DashboardPage />} />
+//           <Route
+//             path="employeeDirectoryPage"
+//             element={<EmployeeDirectoryPage />}
+//           />
+//           <Route
+//             path="employeeManagementPage"
+//             element={<EmployeeManagementPage />}
+//           />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/appBar.js
+++ src/components/appBar.js
@@ -1,60 +1,60 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import Avatar from '@mui/material/Avatar';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import Avatar from "@mui/material/Avatar";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
+    width: "auto"
+  }
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch"
+    }
+  }
 }));
 
 export default function PrimarySearchAppBar() {
@@ -81,19 +81,19 @@ export default function PrimarySearchAppBar() {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right"
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right"
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
@@ -103,19 +103,19 @@ export default function PrimarySearchAppBar() {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right"
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right"
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
@@ -132,7 +132,7 @@ export default function PrimarySearchAppBar() {
         <IconButton
           size="large"
           aria-label="show 17 new notifications"
-          color="inherit"
+          // color="inherit"
         >
           <Badge badgeContent={17} color="error">
             <NotificationsIcon />
@@ -157,27 +157,33 @@ export default function PrimarySearchAppBar() {
 
   return (
     <>
-      <AppBar position="fixed" style={{ maxWidth: `calc(100% - 100px)`, backgroundColor : '#09bbb8' }}>
+      <AppBar
+        position="fixed"
+        style={{ maxWidth: `calc(100% - 100px)`, backgroundColor: "#09bbb8" }}
+      >
         <Toolbar>
-        
-        <Typography
+          <Typography
             variant="h6"
             noWrap
             component="div"
+            style={{ color: "#ffffff" }}
             //sx={{ display: { xs: 'none', sm: 'block' } }}
           >
             Dashboard
           </Typography>
-        
+
           <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-           
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
               color="inherit"
             >
-              <Badge badgeContent={17} color="error">
+              <Badge
+                badgeContent={17}
+                color="error"
+                style={{ color: "#ffffff" }}
+              >
                 <NotificationsIcon />
               </Badge>
             </IconButton>
@@ -190,11 +196,11 @@ export default function PrimarySearchAppBar() {
               onClick={handleProfileMenuOpen}
               color="inherit"
             >
-              <Avatar alt="" src="https://mui.com/static/images/avatar/2.jpg"/>
+              <Avatar alt="" src="https://mui.com/static/images/avatar/2.jpg" />
               {/* <AccountCircle /> */}
             </IconButton>
           </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="show more"
