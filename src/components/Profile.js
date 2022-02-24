d22724aee54f5bf5508c2853c81f6430a5f1d54f
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-02-24 10:27:03 +0530

    changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,11 +4,11 @@ import Typography from "@material-ui/core/Typography";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Drawer from "@material-ui/core/Drawer";
+import Divider from "@mui/material/Divider";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
 import Button from "@mui/material/Button";
-import Divider from "@mui/material/Divider";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import EventIcon from "@mui/icons-material/Event";
@@ -27,41 +27,137 @@ import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
 
 export default function ProfilePage() {
   return (
-    <Grid container spacing={2} style={{ backgroundColor: "#ffffff" }}>
-      <Grid item xs={4} style={{ border: "1px solid" }}>
-        <Grid container>
-          <Grid item xs={6}>
-            User Info
-          </Grid>
-          <Grid item xs={6}>
-            <Link href="#">Edit</Link>
-          </Grid>
-          <Grid item xs={6}>
-            Image
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="body2" gutterBottom>
-              Name
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              Sheeren Wyngaard
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              Employement Number
-            </Typography>
-            <Typography variant="body2" gutterBottom>
-              2
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained">Send Onboarding Package</Button>
-          </Grid>
+    <>
+      {/* <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <div style={{ border: '1px solid'}}>8</div>
         </Grid>
-        <Divider />
+        <Grid item xs={4}>
+        <div style={{ border: '1px solid'}}>4</div>
+        </Grid>
+       
       </Grid>
-      <Grid item xs={8} style={{ border: "1px solid" }}>
-        item2
-      </Grid>
-    </Grid>
+    </Box> */}
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} style={{ backgroundColor: "#ffffff" }}>
+          <Grid item xs={3} style={{ backgroundColor: "" }}>
+            <Grid container>
+              <Grid item xs={6}>
+                <Typography variant="body2" gutterBottom>
+                  User Info
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="body2">
+                  <Link href="#">Edit</Link>
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                Image
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="caption" gutterBottom>
+                  Name
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Sheeren Wyngaard
+                </Typography>
+                <Typography variant="caption" display="block" gutterBottom>
+                  Employement Number
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  2
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Button size="small" variant="contained">
+                  Send Onboarding Package
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Divider />
+              </Grid>
+              <Divider />
+              <Grid item xs={6}>
+                <Typography variant="body2" gutterBottom>
+                  Personal Info
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="body2" gutterBottom>
+                  <Link href="#">Edit</Link>
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="caption" gutterBottom>
+                  Gender
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Nationality
+                </Typography>
+                <Typography variant="caption" gutterBottom>
+                  Birth Date
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Jan 30,2000
+                </Typography>
+                <Typography variant="caption" gutterBottom>
+                  Marital Status
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Married
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="caption" gutterBottom>
+                  Nationality
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  Indian
+                </Typography>
+                <Typography variant="caption" gutterBottom>
+                  Age
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  30 years old
+                </Typography>
+                <Typography variant="caption" gutterBottom>
+                  National ID
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  12345678
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Divider />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body2" gutterBottom>
+                  Contact info
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="caption" gutterBottom>
+                  Email
+                </Typography>
+              </Grid>
+              <Typography variant="subtitle2" gutterBottom>
+                abc@i-see.io
+              </Typography>
+              <Grid item xs={12}>
+                <Typography variant="caption" gutterBottom>
+                  Mobile number
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                  123456679
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={9} style={{ border: "1px solid" }}></Grid>
+        </Grid>
+      </Box>
+    </>
   );
 }
