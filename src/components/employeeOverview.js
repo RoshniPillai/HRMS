3a1d9cfd070586b33666c950cdc2ea5398486d6a
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-02 15:04:42 +0530

    tab changes

--- src/components/employeeOverview.js
+++ src/components/employeeOverview.js
@@ -75,10 +75,21 @@ function a11yProps(index) {
 function EmployeeOverviewPage(props) {
   const { classes } = props;
   const [value, setValue] = React.useState(0);
+  const [value1, setValue1] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const handleChange1 = (event, newValue) => {
+    setValue(newValue + 7);
+  };
+  const handleChange11 = (event, newValue) => {
+    setValue(newValue);
+  };
+  const handleChange2 = (event, newValue) => {
+    setValue(newValue + 3);
+  };
+
   return (
     // <div className={classes.root}>
     //   <PrimarySearchAppBar />
@@ -99,20 +110,73 @@ function EmployeeOverviewPage(props) {
     //     classes={classes.main}
     //   >
     <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+      {/* <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
+        <Tabs value={value} onChange={handleChange11}>
+          <Tab label="Precios" />
+          <Tab label="Usuarios" />
+          <Tab label="Plan" />
+        </Tabs>
+        <Tabs value={value - 3} onChange={handleChange2}>
+          <Tab label="Empleados" />
+        </Tabs>
+      </Box> */}
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          display: "flex",
+          justifyContent: "left",
+          flexWrap: "wrap"
+        }}
+      >
         <Tabs
           value={value}
           onChange={handleChange}
           aria-label="basic tabs example"
+          variant="scrollable"
+          scrollButtons="auto"
+          // style={{ padding: "0 20px" }}
         >
-          <Tab label="Profile" {...a11yProps(0)} />
-          <Tab label="Bank Accounts" {...a11yProps(1)} />
-          <Tab label="Family Info" {...a11yProps(2)} />
-          {/* <Tab label="Item One" {...a11yProps(3)} />
-              <Tab label="Item Two" {...a11yProps(4)} />
-              <Tab label="Item Three" {...a11yProps(5)} /> */}
+          <Tab wrapped label="Profile" {...a11yProps(0)} />
+          <Tab wrapped label="Bank Accounts" {...a11yProps(1)} />
+          <Tab wrapped label="Family Info" {...a11yProps(2)} />
+          <Tab wrapped label="Documents" {...a11yProps(3)} />
+          <Tab wrapped label="Competencies" {...a11yProps(4)} />
+          <Tab wrapped label="Insurance" {...a11yProps(5)} />
+          <Tab wrapped label="Air Ticket Allowance" {...a11yProps(6)} />
+          <Tab wrapped label="TimeOff Setup" {...a11yProps(7)} />
+          <Tab wrapped label="Assets" {...a11yProps(8)} />
+          <Tab wrapped label="Projects" {...a11yProps(9)} />
+          <Tab wrapped label="Part Time Types" {...a11yProps(10)} />
+
+          <Tab wrapped label="Disciplinary Actions" {...a11yProps(11)} />
+          <Tab wrapped label="Schedules" {...a11yProps(12)} />
+          <Tab wrapped label="Contracts" {...a11yProps(13)} />
+          <Tab wrapped label="Custom Fields" {...a11yProps(14)} />
+          <Tab wrapped label="Off-Boarding" {...a11yProps(15)} />
         </Tabs>
       </Box>
+      {/* <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          display: "flex",
+          justifyContent: "left",
+          flexWrap: "wrap"
+        }}
+      >
+        <Tabs
+          value={value - 7}
+          onChange={handleChange1}
+          aria-label="basic tabs example"
+        >
+          <Tab wrapped label="Disciplinary Actions" {...a11yProps(0)} />
+          <Tab wrapped label="Schedules" {...a11yProps(1)} />
+          <Tab wrapped label="Contracts" {...a11yProps(2)} />
+          <Tab wrapped label="Custom Fields" {...a11yProps(3)} />
+          <Tab wrapped label=" Off-Boarding" {...a11yProps(4)} />
+        </Tabs>
+      </Box> */}
       <TabPanel value={value} index={0}>
         <ProfilePage />
       </TabPanel>
@@ -122,15 +186,61 @@ function EmployeeOverviewPage(props) {
       <TabPanel value={value} index={2}>
         <FamilyInfoPage />
       </TabPanel>
-      {/* <TabPanel value={value} index={3}>
-            Item Three
-          </TabPanel>
-          <TabPanel value={value} index={4}>
-            Item Three
-          </TabPanel>
-          <TabPanel value={value} index={5}>
-            Item Three
-          </TabPanel> */}
+      <TabPanel value={value} index={3}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={7}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={8}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={9}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={10}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={11}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={12}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={13}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={14}>
+        content
+      </TabPanel>
+      <TabPanel value={value} index={15}>
+        content
+      </TabPanel>
+
+      {/* <TabPanel value={value1} index={0}>
+        content
+      </TabPanel>
+      <TabPanel value={value1} index={1}>
+        content
+      </TabPanel>
+      <TabPanel value={value1} index={2}>
+        content
+      </TabPanel>
+      <TabPanel value={value1} index={3}>
+        content
+      </TabPanel>
+      <TabPanel value={value1} index={4}>
+        content
+      </TabPanel> */}
     </Box>
     //   </Box>
     // </div>
